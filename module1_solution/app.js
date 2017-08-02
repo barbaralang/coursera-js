359ab2d9768775fd90239730efb2290abf2f1264
barbaralang <babsi.lang@gmx.at>
2017-08-02 22:55:18 +0200

    initial  commit

---
+++ module1_solution/app.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheckApp', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope){
+
+      var enjoy = 'Enjoy!';
+      var much = 'Too much!';
+      var emptyData = 'Please enter data first';
+      var danger = 'danger';
+      var success = 'success';
+
+      $scope.state = danger;
+      $scope.lunchMessage = '';
+
+      $scope.checkLunch = function() {
+
+        if($scope.dishList != null){
+          var size = $scope.dishList.split(',').length;
+          if(size <= 3){
+            $scope.lunchMessage = enjoy;
+            $scope.state = success;
+          }else{
+            $scope.lunchMessage = much;
+            $scope.state = danger;
+          }
+        }else{
+          $scope.lunchMessage = emptyData;
+          $scope.state = danger;
+        }
+        
+      }
+
+    }
+
+})();
